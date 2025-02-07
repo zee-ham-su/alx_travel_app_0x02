0e0a57d241ab37770c5c1cf3fd0c7e06e4ddd70c
zee-ham-su <hsufiian@yahoo.com>
2025-02-07 08:20:34 +0000

    Add payment initiation and verification endpoints, and create Payment model

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path, include
+from django.urls import path
+from .views import InitiatePaymentView, VerifyPaymentView, ListingViewSet, BookingViewSet
 from rest_framework.routers import DefaultRouter
-from .views import ListingViewSet, BookingViewSet
 
 router = DefaultRouter()
 router.register(r'listings', ListingViewSet)
 router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('initiate-payment/', InitiatePaymentView.as_view(),
+         name='initiate-payment'),
+    path('verify-payment/', VerifyPaymentView.as_view(), name='verify-payment'),
 ]
+
+urlpatterns += router.urls
